71f845f2fbb31081508adb1c731e714ec03f2c68
Kyumin Lee <kyumin133@gmail.com>
2017-03-14 20:53:31 -0700

    display error messages in user auth forms

--- frontend/components/user_auth/session_form.jsx
+++ frontend/components/user_auth/session_form.jsx
@@ -9,7 +9,8 @@ class SessionForm extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.state = {
       email: this.props.email,
-      password: ""
+      password: "",
+      errors: []
     }
   }
 
@@ -26,19 +27,29 @@ class SessionForm extends React.Component {
   handleSubmit(e) {
     e.preventDefault();
     this.props.processForm(this.state, this.props.formType).then(() => {}).fail(() => {
-      console.log(this.props.errors);
+      this.setState({errors: this.props.errors})
     });
   }
 
   render() {
     let action = (this.props.formType === "signup") ? "Sign Up" : "Log In"
+    let errors = "";
+    let passwordClass = "box";
+    if (this.state.errors.length > 0) {
+      let errorArr = this.state.errors.map((error, i) => {
+        return <li key={i}>{error}</li>
+      });
+      errors = <ul className="error-ul">{errorArr}</ul>;
+      passwordClass = "error-box";
+    }
     return  <div className="auth-div" onClick={(e) => {e.stopPropagation();}}>
               <form onSubmit={this.handleSubmit}>
                 <span className="email-container box" onClick={this.returnToEmailEntry}>
                   <i className="fa fa-chevron-left" aria-hidden="true"></i>
                   <span className="email">{this.state.email}</span>
                 </span>
-                <input className="box" autoFocus type="password" value={this.state.password} placeholder="Your password" onChange={this.update("password")}></input>
+                <input className={passwordClass} autoFocus type="password" value={this.state.password} placeholder="Your password" onChange={this.update("password")}></input>
+                {errors}
                 <button type="submit">{action}</button>
               </form>
             </div>;
