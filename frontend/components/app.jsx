87472fdf688f679c48de8060aeeea49690baa845
Kyumin Lee <kyumin133@gmail.com>
2017-03-20 20:09:49 -0700

    change logo

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -15,7 +15,7 @@ class App extends React.Component {
   render() {
     let header = <header>
       <div className="logo" onClick={this.goHome}>
-        <img src="assets/logo.svg"></img>
+        <img src="assets/cloud.png"></img>
       </div>
       < NavBarContainer />
     </header>;
@@ -23,7 +23,7 @@ class App extends React.Component {
     let footer = null;
     if (!window.currentUser) {
       footer = <footer>
-        <div className="img-src">Icons made by <a href="http://www.freepik.com" title="Freepik">Freepik</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a></div>
+        <div className="img-src">Icon made by <a href="http://www.flaticon.com/authors/anhgreen" title="anhgreen">anhgreen</a> from <a href="http://www.flaticon.com" title="Flaticon">www.flaticon.com</a></div>
         <div className="img-src">
           Music provided by <a href="freemusicpublicdomain.com"> Exzel Music Publishing </a> <br />
           Licensed under <a href="http://creativecommons.org/licenses/by/3.0/">Creative Commons</a>: By Attribution 3.0 <br />
